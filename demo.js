a89a0043dfb5151db358e78606811576f3067bcf
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2014-11-16 09:18:22 -0600

    more improvements

--- demo.js
+++ demo.js
@@ -1,20 +1,19 @@
 'use strict';
-/*global $: false */
+/*global $: false, document: false */
 
 function loadTable() {
-  var col, colCount = 20, row, rowCount = 40;
+  var bodyRow, col, colCount = 20, row, rowCount = 40;
   var headRow = $('#t1 > thead > tr');
   var body = $('#t2 > tbody');
 
   for (col = 0; col < colCount; col++) {
-    var th = headRow.append('<th><div>column ' + col + '</div></th>');
+    headRow.append('<th><div>column ' + col + '</div></th>');
   }
 
   for (row = 0; row < rowCount; row++) {
-    var bodyRow = body.append('<tr>');
+    bodyRow = body.append('<tr>');
     for (col = 0; col < colCount; col++) {
-      var td = bodyRow.append(
-        '<td><div>cell ' + row + ', ' + col + '</div></td>');
+      bodyRow.append('<td><div>cell ' + row + ', ' + col + '</div></td>');
     }
   }
 }
@@ -22,9 +21,9 @@ function loadTable() {
 $(function () {
   loadTable();
 
-  var t1 = $('#t1');
-  var t2 = $('#t2');
-  t2.on('scroll', function () {
-    t1[0].scrollLeft = t2[0].scrollLeft;
+  var t1 = document.getElementById('t1');
+  var t2 = document.getElementById('t2');
+  t2.addEventListener('scroll', function () {
+    t1.scrollLeft = t2.scrollLeft;
   });
 });
